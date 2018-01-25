6cfec584e1e100487c21c8f940e6892d3e5e76f2
mlch911 <mlch911@mluoc.tech>
2018-01-25 07:01:56 -0600

    0.0.1
    Initialization test.

---
+++ ss-node-script.sh
@@ -0,0 +1,270 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#=================================================
+#	System Required: CentOS 7+
+#	Description: sspanel后端一键安装脚本
+#	Version: 0.0.1
+#	Author: 壕琛
+#	Blog: http://mluoc.top/
+#=================================================
+
+sh_ver="0.0.1"
+github="raw"
+
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[信息]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+
+#############开始#############
+check_sys
+check_version
+[[ ${release} != "centos" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} !" && exit 1
+start_menu
+#############开始#############
+
+
+#开始菜单
+start_menu(){
+clear
+echo && echo -e " sspanel后端 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+  -- 壕琛小站 | ss.mluoc.tk --
+  
+ ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
+ ${Green_font_prefix}1.${Font_color_suffix} 安装脚本
+ ${Green_font_prefix}2.${Font_color_suffix} 卸载脚本
+ ${Green_font_prefix}3.${Font_color_suffix} 退出脚本
+————————————————————————————————" && echo
+
+	# check_status
+	# if [[ ${kernel_status} == "noinstall" ]]; then
+		# echo -e " 当前状态: ${Green_font_prefix}未安装${Font_color_suffix} 加速内核 ${Red_font_prefix}请先安装内核${Font_color_suffix}"
+	# else
+		# echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} ${_font_prefix}${kernel_status}${Font_color_suffix} 加速内核 , ${Green_font_prefix}${run_status}${Font_color_suffix}"	
+	# fi
+	
+	
+echo
+read -p " 请输入数字 [0-8]:" num
+case "$num" in
+	0)
+	Update_Shell
+	;;
+	1)
+	Install_Shell
+	;;
+	2)
+	Uninstall_Shell
+	;;
+	3)
+	exit 1
+	;;
+	*)
+	clear
+	echo -e "${Error}:请输入正确数字 [0-8]"
+	sleep 5s
+	start_menu
+	;;
+esac
+}
+
+#更新脚本
+Update_Shell(){
+	echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
+	sh_new_ver=$(wget --no-check-certificate -qO- "http://${github}/tcp.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && start_menu
+	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
+		echo -e "发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
+		read -p "(默认: y):" yn
+		[[ -z "${yn}" ]] && yn="y"
+		if [[ ${yn} == [Yy] ]]; then
+			wget -N --no-check-certificate http://${github}/tcp.sh && chmod +x tcp.sh
+			echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !"
+		else
+			echo && echo "	已取消..." && echo
+			start_menu
+		fi
+	else
+		echo -e "当前已是最新版本[ ${sh_new_ver} ] !"
+		sleep 5s
+		start_menu
+	fi
+}
+
+#安装脚本
+Install_Shell(){
+	if [[ "${release}" == "centos" ]]; then
+		cd ~
+		yum -y groupinstall "Development Tools"
+		wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+		tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+		./configure && make -j2 && make install
+		echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+		ldconfig
+		cd /root
+		yum -y install python-setuptools
+		easy_install pip
+		git clone -b manyuser https://github.com/glzjin/shadowsocks.git
+		cd shadowsocks
+		pip install -r requirements.txt
+		cp apiconfig.py userapiconfig.py
+		cp config.json user-config.json
+	fi
+	
+	
+	
+	echo -e "${Info}sspanel后端安装成功！"
+}
+
+
+
+#############系统检测组件#############
+
+#检查系统
+check_sys(){
+	if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+}
+
+#检查Linux版本
+check_version(){
+	if [[ -s /etc/redhat-release ]]; then
+		version=`grep -oE  "[0-9.]+" /etc/redhat-release | cut -d . -f 1`
+	else
+		version=`grep -oE  "[0-9.]+" /etc/issue | cut -d . -f 1`
+	fi
+	bit=`uname -m`
+	if [[ ${bit} = "x86_64" ]]; then
+		bit="x64"
+	else
+		bit="x32"
+	fi
+}
+
+#检查安装bbr的系统要求
+# check_sys_bbr(){
+	# check_version
+	# if [[ "${release}" == "centos" ]]; then
+		# if [[ ${version} -ge "6" ]]; then
+			# installbbr
+		# else
+			# echo -e "${Error} BBR内核不支持当前系统 ${release} ${version} ${bit} !" && exit 1
+		# fi
+	# elif [[ "${release}" == "debian" ]]; then
+		# if [[ ${version} -ge "8" ]]; then
+			# installbbr
+		# else
+			# echo -e "${Error} BBR内核不支持当前系统 ${release} ${version} ${bit} !" && exit 1
+		# fi
+	# elif [[ "${release}" == "ubuntu" ]]; then
+		# if [[ ${version} -ge "14" ]]; then
+			# installbbr
+		# else
+			# echo -e "${Error} BBR内核不支持当前系统 ${release} ${version} ${bit} !" && exit 1
+		# fi
+	# else
+		# echo -e "${Error} BBR内核不支持当前系统 ${release} ${version} ${bit} !" && exit 1
+	# fi
+# }
+
+#检查安装Lotsever的系统要求
+# check_sys_Lotsever(){
+	# check_version
+	# if [[ "${release}" == "centos" ]]; then
+		# if [[ ${version} == "6" ]]; then
+			# kernel_version="2.6.32-504"
+			# installlot
+		# elif [[ ${version} == "7" ]]; then
+			# yum -y install net-tools
+			# kernel_version="3.10.0-327"
+			# installlot
+		# else
+			# echo -e "${Error} Lotsever不支持当前系统 ${release} ${version} ${bit} !" && exit 1
+		# fi
+	# elif [[ "${release}" == "debian" ]]; then
+		# if [[ ${version} -ge "7" ]]; then
+			# if [[ ${bit} == "x64" ]]; then
+				# kernel_version="3.16.0-4"
+				# installlot
+			# elif [[ ${bit} == "x32" ]]; then
+				# kernel_version="3.2.0-4"
+				# installlot
+			# fi
+		# else
+			# echo -e "${Error} Lotsever不支持当前系统 ${release} ${version} ${bit} !" && exit 1
+		# fi
+	# elif [[ "${release}" == "ubuntu" ]]; then
+		# if [[ ${version} -ge "12" ]]; then
+			# if [[ ${bit} == "x64" ]]; then
+				# kernel_version="4.4.0-47"
+				# installlot
+			# elif [[ ${bit} == "x32" ]]; then
+				# kernel_version="3.13.0-29"
+				# installlot
+			# fi
+		# else
+			# echo -e "${Error} Lotsever不支持当前系统 ${release} ${version} ${bit} !" && exit 1
+		# fi
+	# else
+		# echo -e "${Error} Lotsever不支持当前系统 ${release} ${version} ${bit} !" && exit 1
+	# fi
+# }
+
+# check_status(){
+	# kernel_version=`uname -r | awk -F "-" '{print $1}'`
+	# if [[ ${kernel_version} = "4.11.8" ]]; then
+		# kernel_status="BBR"
+	# elif [[ ${kernel_version} = "3.10.0" || ${kernel_version} = "3.16.0" || ${kernel_version} = "3.2.0" || ${kernel_version} = "4.4.0" || ${kernel_version} = "3.13.0"  || ${kernel_version} = "2.6.32" ]]; then
+		# kernel_status="Lotserver"
+	# else 
+		# kernel_status="noinstall"
+	# fi
+	# if [[ ${kernel_status} == "Lotserver" ]]; then
+		# if [[ -e /appex/bin/serverSpeeder.sh ]]; then
+			# run_status=`bash /appex/bin/serverSpeeder.sh status | grep "ServerSpeeder" | awk  '{print $3}'`
+			# if [[ ${run_status} = "running!" ]]; then
+				# run_status="启动成功"
+			# else 
+				# run_status="启动失败"
+			# fi
+		# else 
+			# run_status="未安装加速模块"
+		# fi
+	# elif [[ ${kernel_status} == "BBR" ]]; then
+		# run_status=`grep "net.ipv4.tcp_congestion_control" /etc/sysctl.conf | awk -F "=" '{print $2}'`
+		# if [[ ${run_status} == "bbr" ]]; then
+			# run_status=`lsmod | grep "bbr" | awk '{print $1}'`
+			# if [[ ${run_status} == "tcp_bbr" ]]; then
+				# run_status="BBR启动成功"
+			# else 
+				# run_status="BBR启动失败"
+			# fi
+		# elif [[ ${run_status} == "tsunami" ]]; then
+			# run_status=`lsmod | grep "tsunami" | awk '{print $1}'`
+			# if [[ ${run_status} == "tcp_tsunami" ]]; then
+				# run_status="BBR魔改版启动成功"
+			# else 
+				# run_status="BBR魔改版启动失败"
+			# fi
+		# else 
+			# run_status="未安装加速模块"
+		# fi
+	# fi
+# }
+
+#############系统检测组件#############
