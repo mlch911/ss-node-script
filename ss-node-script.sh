6fcd69c96ee9916bac020f54e18523f0bdf6051b
mlch911 <mlch911@mluoc.tech>
2018-06-20 03:55:09 +0800

    Ver 0.4.7
    bug fix

--- ss-node-script.sh
+++ ss-node-script.sh
@@ -5,12 +5,12 @@ export PATH
 #=================================================
 #	System Required: CentOS 7+
 #	Description: sspanel后端一键安装脚本
-#	Version: 0.4.6
+#	Version: 0.4.7
 #	Author: 壕琛
 #	Blog: http://mluoc.top/
 #=================================================
 
-sh_ver="0.4.6"
+sh_ver="0.4.7"
 github="raw.githubusercontent.com/mlch911/ss-node-script/master/"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
@@ -23,10 +23,10 @@ Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 #开始菜单
 start_menu(){
 clear
-sh_new_ver=$(wget --no-check-certificate -qO- "https://${github}/ss-node-script.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
-if [[ ${sh_new_ver} != ${sh_ver} ]]; then
-	Update_Shell
-fi
+# sh_new_ver=$(wget --no-check-certificate -qO- "https://${github}/ss-node-script.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+# if [[ ${sh_new_ver} != ${sh_ver} ]]; then
+# 	Update_Shell
+# fi
 echo && echo -e " sspanel后端 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   -- 壕琛小站 | cc.mluoc.tk --
 
