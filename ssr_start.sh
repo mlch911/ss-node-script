0b58169b98113b2289aa95de43cd01722d139ac2
mlch911 <mlch911@mluoc.tech>
2018-06-24 01:25:19 +0800

    Ver 0.5.3
    bug fix

--- ssr_start.sh
+++ ssr_start.sh
@@ -1,6 +1,6 @@
 screen_name="ssr"
 screen -dmS $screen_name
 
-cmd="python /root/shadowsocks/server";
+cmd="python /root/shadowsocks/server.py";
 screen -x -S $screen_name -p 0 -X stuff "$cmd"
 screen -x -S $screen_name -p 0 -X stuff '\n'
